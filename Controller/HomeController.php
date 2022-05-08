b9902c2ad3906cdb247a868a2eb35304f8fec971
zabaramaxim <90805053+zabaramaxim@users.noreply.github.com>
2022-05-09 00:06:11 +0300

    Update HomeController.php

--- Controller/HomeController.php
+++ Controller/HomeController.php
@@ -10,7 +10,6 @@ class HomeController extends BaseController
     {
         $shining = new BookCollection();
         $shining = $shining->query("Select * from book where title = 'Shining'");
-        print_r($shining);
         $controllerName = $this->getClassName();
         $this->initTwig();
         return $this->twig->render('home/index.html.twig', ['name' => $controllerName, 'path' => __DIR__ . '/' . $controllerName]);
